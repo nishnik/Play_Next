bea04ea2016dbc6435f2221296a9d59be53bd5fc
Nishant <nishantiam@gmail.com>
2017-01-25 15:31:14 +0530

    Added communication and implementing queue

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,18 +1,3 @@
-var download_links = document.querySelectorAll('a[class=" content-link spf-link  yt-uix-sessionlink      spf-link "]');
-
-if(download_links.length){
-	for (var i = 0; i < download_links.length; i++) {
-		var link = download_links[i];
-		var p = document.createElement('p');
-			p.innerHTML = '<a href="#">Play Next</a>';
-			p.className = "button play-next";
-			link.parentElement.insertAdjacentElement('afterbegin',p);
-			p.dataset.name = link.href;
-			p.querySelector('a').addEventListener('click',clickHandler,true);
-	}
-}
-
-
 (function(document) {
     "use strict";
     var s = document.createElement("script");
@@ -25,12 +10,55 @@ if(download_links.length){
     document.documentElement.appendChild(s)
 })(window.document);
 
+var queue = [];
+
+function insertButton() {
+    console.log("insertButton act");
+    var buttons = document.querySelectorAll('p[class="button play-next"]');
+    console.log(buttons.length);
+    if (buttons.length == 0) {
+        var download_links = document.querySelectorAll('a[class=" content-link spf-link  yt-uix-sessionlink      spf-link "]');
+        console.log(download_links.length);
+        if(download_links.length){
+        	for (var i = 0; i < download_links.length; i++) {
+        		var link = download_links[i];
+        		var p = document.createElement('p');
+        			p.innerHTML = '<a href="#">Play Next</a>';
+        			p.className = "button play-next";
+        			link.parentElement.insertAdjacentElement('afterbegin',p);
+        			p.dataset.name = link.href;
+        			p.querySelector('a').addEventListener('click',clickHandler,true);
+        	}
+        }
+    }
+}
 
 function clickHandler(e){
-	var actualCode = `var link = "${ this.parentNode.dataset.name }";`;
-	console.log(actualCode);
-	var script = document.createElement('script');
-	script.textContent = actualCode;
-	document.documentElement.appendChild(script);
-	script.remove();
+    window.queue.push(this.parentNode.dataset.name);
+    var codeToPush = `var queue = "${ window.queue }";`;
+    console.log(codeToPush);
+    var script = document.createElement('script');
+    script.textContent = actualCode;
+    document.documentElement.appendChild(script);
+    script.remove();
 }
+// At the very start add the buttons
+insertButton();
+
+var port = chrome.runtime.connect();
+
+// YouTube is lesser prone to disturbances than sadness
+window.addEventListener("message", function(event) {
+  // Only accept messages from ourselves
+  if (event.source != window)
+    return;
+
+  if (event.data.type && (event.data.type == "FROM_PAGE")) {
+    insertButton();
+    console.log(window.queue);
+    console.log("Content script received: " + event.data.text);
+    if (event.data.text == "0") {
+        window.open(link,"_self");
+    }
+  }
+}, false);
--- src/inject/main.js
+++ src/inject/main.js
@@ -2,7 +2,8 @@ player = document.getElementById('movie_player');
 player.addEventListener("onStateChange", "onPlayerStateChanged");
 
 
-var link = "nothing";
+
+var queue = [];
 function onPlayerStateChanged(newState) {
 // -1 (unstarted)
 // 0 (ended)
@@ -10,9 +11,14 @@ function onPlayerStateChanged(newState) {
 // 2 (paused)
 // 3 (buffering)
 // 5 (video cued).
+	window.postMessage({ type: "FROM_PAGE", text: newState }, "*");
     console.log(link);
+    console.log(link.substring(32))
     console.log(newState);
     if (newState == 0 && link != "nothing") {
-        window.open(link,"_self")
+        window.open(link,"_self");
     }
+    // if (newState == 2) {
+    // 	 window.postMessage({ type: "FROM_PAGE", text: "Hello from the webpage!" }, "*");
+    // }
 }
