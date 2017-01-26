78da6c01cd709905bd82f387ee6c9b98b0cc9b5c
Nishant <nishantiam@gmail.com>
2017-01-27 01:46:17 +0530

    Added communication between content and popup

--- popup.js
+++ popup.js
@@ -1,1 +1,43 @@
 // TODO
+ // chrome.tabs.query({active: true, currentWindow: true},function(tabs) {
+ //                     chrome.tabs.sendMessage(tabs[0].id, {message: "hello"});});
+
+function setDOMInfo(info) {
+    var queue = JSON.parse(info);
+    console.log(queue);
+  //   for (var i = 0; i <= queue.length; ++i) {
+		// var p = document.createElement('p');
+		// p.innerHTML = '<a href="';
+		// p.innerHTML.concat(queue[i][0], '">', queue[i][1], '</a>');
+		// p.className = "song-queue";
+		// document.getElementById('content').appendChild(p);
+  //   }
+
+  document.getElementById('content').textContent = queue;
+}
+
+// Once the DOM is ready...
+window.addEventListener('DOMContentLoaded', function () {
+  // ...query for the active tab...
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function (tabs) {
+    // ...and send a request for the DOM info...
+    chrome.tabs.sendMessage(
+        tabs[0].id,
+        {from: 'popup', subject: 'DOMInfo'},
+        // ...also specifying a callback to be called 
+        //    from the receiving end (content script)
+        setDOMInfo);
+  });
+});
+
+var p = document.createElement('p');
+console.log(localStorage);
+p.innerHTML = '<a href="#"><i>Play Next</i></a>';
+p.className = "button play-next";
+window.postMessage({ type: "FROM_PAGE", text: "popup" }, "*");
+// link.parentElement.insertAdjacentElement('afterbegin',p);
+// p.dataset.name = link.href;
+// p.querySelector('a').addEventListener('click',clickHandler,true);
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -10,14 +10,18 @@
 })(window.document);
 
 var to_match = 'a[class="';
+var LOC_HREF = 0;
 if (document.location.href == "https://www.youtube.com/") {
     to_match = to_match.concat(" yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink      spf-link ", '"]');
+    LOC_HREF = 1;
 }
 else if (document.location.href.substring(0, 29) == "https://www.youtube.com/watch") {
     to_match = to_match.concat(" content-link spf-link  yt-uix-sessionlink      spf-link ", '"]');
+    LOC_HREF = 2;
 }
 else if (document.location.href.substring(0, 31) == "https://www.youtube.com/results") {
     to_match = to_match.concat("yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink      spf-link ", '"]');
+    LOC_HREF = 3;
 }
 
 var queue = [];
@@ -34,24 +38,17 @@ function insertButton() {
     var buttons = document.querySelectorAll('p[class="button play-next"]');
     var download_links = document.querySelectorAll(to_match);
     if(download_links.length != buttons.length){
-        // TODO: check if we iterate in reverse manner, can we exit the loop soon (only one check)
-        for (var i = 0; i < download_links.length; ++i) {
+        for (var i = download_links.length-1; i >=buttons.length ; --i) {
             var link = download_links[i];
-            var to_insert = true;
-            for (var j = 0; j < buttons.length; ++i) {
-                var button = buttons[j];
-                if (button.dataset.name == link.href) {
-                    to_insert = false;
-                    break;
-                }
-            }
-            if (to_insert == false)
-                continue;
             var p = document.createElement('p');
-            p.innerHTML = '<a href="#"><i>Play Next</i></a>';
+            p.innerHTML = '<a><i>Play Next</i></a>';
             p.className = "button play-next";
             link.parentElement.insertAdjacentElement('afterbegin',p);
             p.dataset.name = link.href;
+            if (LOC_HREF == 2)
+                p.dataset.song_name = link.querySelectorAll('span[class="title"]')[0].innerText;
+            if (LOC_HREF == 1)
+                p.dataset.song_name = link.innerText;
             p.querySelector('a').addEventListener('click',clickHandler,true);
         }
     }
@@ -61,20 +58,20 @@ function insertButton() {
 function insertPlayInfo() { // only called by sendLink
     if (window.queue.length > 0) {
         tmp = 'p[data-name="';
-        tmp = tmp.concat(window.queue[0]);
+        tmp = tmp.concat(window.queue[0][0]);
         tmp = tmp.concat('"');
         var next_song = document.querySelectorAll(tmp);
         if (next_song.length > 0) {
-            next_song[0].innerHTML = '<a href="#" style="color: blue"><i>Playing Next</i></a>';
+            next_song[0].innerHTML = '<a style="color: blue"><i>Playing Next</i></a>';
         }
         for (var i = 1; i < window.queue.length; ++i) {
             tmp = 'p[data-name="';
-            tmp = tmp.concat(window.queue[i]);
+            tmp = tmp.concat(window.queue[i][0]);
             tmp = tmp.concat('"');
             var que_song = document.querySelectorAll(tmp);
             if (que_song.length > 0) {
-                var tmp = '<a href="" style="color: blue"><i>In Queue at #';
-                tmp = tmp.concat(i.toString(), '</i></a>');
+                var tmp = '<a style="color: blue"><i>In Queue at #';
+                tmp = tmp.concat((i+1).toString(), '</i></a>');
                 que_song[0].innerHTML = tmp;
             }      
         }
@@ -87,14 +84,14 @@ function clickHandler(e){
     if (localStorage.getItem(save_address) != null) {
         queue = JSON.parse(localStorage[save_address]);
     }
-    window.queue.push(this.parentNode.dataset.name);
+    window.queue.push([this.parentNode.dataset.name, this.parentNode.dataset.song_name]);
     localStorage[save_address] = JSON.stringify(window.queue);
     sendLink();
 }
 
 function sendLink() {
     if (queue.length > 0) {
-        var codeToPush = `var link = "${ window.queue[0] }";`;
+        var codeToPush = `var link = "${ window.queue[0][0] }";`;
         var script = document.createElement('script');
         script.textContent = codeToPush;
         document.documentElement.appendChild(script);
@@ -102,8 +99,6 @@ function sendLink() {
     }
     insertPlayInfo();
 }
-
-
 
 // YouTube is lesser prone to disturbances than sadness
 window.addEventListener("message", function(event) {
@@ -117,9 +112,39 @@ window.addEventListener("message", function(event) {
     }
     insertButton();
     console.log(window.queue);
+    console.log(event.data.text);
     if (event.data.text == "pop") {
         window.queue.shift();
         localStorage[save_address] = JSON.stringify(window.queue);
     }
   }
 }, false);
+
+
+// tmp();
+// function tmp() {
+//     console.log("in tmp())");
+// var tabId;
+// chrome.extension.sendMessage({ type: 'getTabId' }, function(res) {
+//     tabId = res.tabId;
+//     console.log(tabId);
+// });
+// }
+
+chrome.runtime.onMessage.addListener(function (msg, sender, response) {
+  // First, validate the message's structure
+  if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
+    if (localStorage.getItem(save_address) != null) {
+        window.queue = JSON.parse(localStorage[save_address]);
+    }
+
+    // Collect the necessary data 
+    // (For your specific requirements `document.querySelectorAll(...)`
+    //  should be equivalent to jquery's `$(...)`)
+    var domInfo = JSON.stringify(window.queue);
+
+    // Directly respond to the sender (popup), 
+    // through the specified callback */
+    response(domInfo);
+  }
+});
